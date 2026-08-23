211207b842c880dbb7d8cef2deae5dab28c5081e
abmb1764-create <abmb1764@gmail.com>
2026-08-23 09:48:24 +0530

    project updated

--- atm.py
+++ atm.py
@@ -1,24 +1,26 @@
 while True:
     person=input("Enter your name")
-    amount=int(input("Hello", person, "Enter the withdrawl amount"))
-if amount<=0:
-    print("Ivalid amount, try again")
-else:
-    print("Dispensing", amount, "for", person)
-remain=amount
-i=1
-while i<=5:
-    if i==1:value=100
-    elif i==2:value=50
-    elif i==3:value=20
-    elif i==4:value=10
-    elif i==5:value=1
-    count=amount//value
-    if count>0:
-        print(count, " notes of", value, " dispensed")
-        remaining=remaining%value
+    amount=int(input(f"Hello {person} Enter the withdrawl amount"))
+    if amount<=0:
+        print("Ivalid amount, try again")
+    else:
+        print("Dispensing", amount, "for", person)
+    remain=amount
+    i=1
+    while i<=5:
+
+        if i==1:value=100
+        elif i==2:value=50
+        elif i==3:value=20
+        elif i==4:value=10
+        else:value=1
+        count=remain//value
+        if count>0:
+            print(count, " notes of", value, " dispensed")
+            remain=remain%value
+        i+=1
     anwser=input(" Next user? (y/n)")
-    if awnser.lower()=="y":
+    if anwser.lower()=="y":
         continue
     else:
         break
